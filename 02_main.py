ecb034d85d8accc21398c65ed6f86779aa78ebb7
Zane <z.pearton@accenture.com>
2023-08-27 15:44:15 +1000

    revision

--- 02_main.py
+++ 02_main.py
@@ -4,14 +4,21 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
+
 class GAN:
     def __init__(self, noise_dim):
         self.noise_dim = noise_dim
         self.generator = self.build_generator()
         self.discriminator = self.build_discriminator()
-        self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-        self.generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
-        self.discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
+        self.cross_entropy = tf.keras.losses.BinaryCrossentropy(
+            from_logits=True)
+        # self.generator_optimizer = tf.keras.optimizers.Adam(
+        #     learning_rate=0.0002, beta_1=0.5)
+        # self.discriminator_optimizer = tf.keras.optimizers.Adam(
+        #     learning_rate=0.0002, beta_1=0.5)
+        self.generator_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5)
+        self.discriminator_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5)
+
         # TensorBoard setup
         self.log_dir = 'logs/'
         self.train_summary_writer = tf.summary.create_file_writer(self.log_dir)
@@ -33,9 +40,11 @@ class GAN:
     def build_discriminator(self):
         model = tf.keras.Sequential([
             tf.keras.layers.Input(shape=(32, 32, 3)),
-            tf.keras.layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', activation='relu'),
+            tf.keras.layers.Conv2D(64, (3, 3), strides=(
+                2, 2), padding='same', activation='relu'),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', activation='relu'),
+            tf.keras.layers.Conv2D(128, (3, 3), strides=(
+                2, 2), padding='same', activation='relu'),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1)  # No activation function here
@@ -50,12 +59,14 @@ class GAN:
         fake_loss = self.cross_entropy(tf.zeros_like(fake_output), fake_output)
         return real_loss + fake_loss
 
+
 def load_and_preprocess_data(filename):
     with open(filename, 'rb') as f:
         batch = pickle.load(f, encoding='bytes')
     data = batch[b'data'].astype(np.float32) / 255.0
     data = data.reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
     return data
+
 
 def visualize_progress(epoch, generator, noise_dim):
     noise = tf.random.normal([16, noise_dim])
@@ -66,39 +77,57 @@ def visualize_progress(epoch, generator, noise_dim):
         ax.axis('off')
     plt.show()
 
+
 if __name__ == "__main__":
     noise_dim = 100
+    batchsize = 5
     data = load_and_preprocess_data('dat/data_batch_2')
-    dataset = tf.data.Dataset.from_tensor_slices(data).shuffle(len(data)).batch(64, drop_remainder=True)
+    dataset = tf.data.Dataset.from_tensor_slices(
+        data).shuffle(len(data)).batch(batchsize, drop_remainder=True)
 
     gan = GAN(noise_dim)
     step_counter = 0
     epochs = 100
-for epoch in range(epochs): 
-    for batch in dataset:
-        batch_size = batch.shape[0]
-        noise = tf.random.normal([batch_size, noise_dim])
+    for epoch in range(epochs):
+        disc_loss_sum = 0  # Initialize disc_loss_sum at the start of each epoch
+        gen_loss_sum = 0   # Initialize gen_loss_sum at the start of each epoch
 
-        with tf.GradientTape() as disc_tape, tf.GradientTape() as gen_tape:
-            generated_images = gan.generator(noise, training=True)
-            real_output = gan.discriminator(batch, training=True)
-            fake_output = gan.discriminator(generated_images, training=True)
+        for batch in dataset:
+            batch_size = batch.shape[0]
+            noise = tf.random.normal([batch_size, noise_dim])
 
-            gen_loss = gan.generator_loss(fake_output)
-            disc_loss = gan.discriminator_loss(real_output, fake_output)
+            with tf.GradientTape() as disc_tape, tf.GradientTape() as gen_tape:
+                generated_images = gan.generator(noise, training=True)
+                real_output = gan.discriminator(batch, training=True)
+                fake_output = gan.discriminator(generated_images, training=True)
 
-        # Log to TensorBoard
-        with gan.train_summary_writer.as_default():
-            tf.summary.scalar('Generator Loss', gen_loss, step=step_counter)
-            tf.summary.scalar('Discriminator Loss', disc_loss, step=step_counter)
+                gen_loss = gan.generator_loss(fake_output)
+                disc_loss = gan.discriminator_loss(real_output, fake_output)
 
-        step_counter += 1
+                disc_loss_sum += disc_loss  # Accumulate the disc_loss
+                gen_loss_sum += gen_loss    # Accumulate the gen_loss
 
-        gradients_of_generator = gen_tape.gradient(gen_loss, gan.generator.trainable_variables)
-        gradients_of_discriminator = disc_tape.gradient(disc_loss, gan.discriminator.trainable_variables)
+            # Log to TensorBoard
+            with gan.train_summary_writer.as_default():
+                tf.summary.scalar('Generator Loss', gen_loss, step=step_counter)
+                tf.summary.scalar('Discriminator Loss',
+                                disc_loss, step=step_counter)
 
-        gan.generator_optimizer.apply_gradients(zip(gradients_of_generator, gan.generator.trainable_variables))
-        gan.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, gan.discriminator.trainable_variables))
+            step_counter += 1
 
-    print(f"Epoch {epoch}/{epochs}, Disc Loss: {disc_loss.numpy()}, Gen Loss: {gen_loss.numpy()}")
-    visualize_progress(epoch, gan.generator, noise_dim)
+            gradients_of_generator = gen_tape.gradient(
+                gen_loss, gan.generator.trainable_variables)
+            gradients_of_discriminator = disc_tape.gradient(
+                disc_loss, gan.discriminator.trainable_variables)
+
+            gan.generator_optimizer.apply_gradients(
+                zip(gradients_of_generator, gan.generator.trainable_variables))
+            gan.discriminator_optimizer.apply_gradients(
+                zip(gradients_of_discriminator, gan.discriminator.trainable_variables))
+
+        # Compute the average losses for the epoch
+        avg_disc_loss = disc_loss_sum / len(dataset)
+        avg_gen_loss = gen_loss_sum / len(dataset)
+
+        print(f"Epoch {epoch}/{epochs}, Avg Disc Loss: {avg_disc_loss.numpy()}, Avg Gen Loss: {avg_gen_loss.numpy()}")
+        visualize_progress(epoch, gan.generator, noise_dim)
